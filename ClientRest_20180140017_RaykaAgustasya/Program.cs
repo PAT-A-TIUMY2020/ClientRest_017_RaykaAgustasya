c67281409cffbf3dfd2e175a3c801f8a7fda99e8
Rayka Agustasya <raykaagustasya16@gmail.com>
2020-12-21 21:54:05 +0700

    Add project files.

---
+++ ClientRest_20180140017_RaykaAgustasya/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using System.Runtime.Serialization;
+
+namespace ClientRest_20180140017_RaykaAgustasya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
